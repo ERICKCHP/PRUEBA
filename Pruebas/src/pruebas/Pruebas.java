71a9aeb9b81f0d9e9662b5433d70c2b53369b44c
Erickch <erickchp@hotmail.com>
2021-04-28 21:45:51 -0500

    Cambio 1 Erick

--- Pruebas/src/pruebas/Pruebas.java
+++ Pruebas/src/pruebas/Pruebas.java
@@ -15,7 +15,9 @@ public class Pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        System.out.println("1) Erick ");
+    
     }
     
 }
